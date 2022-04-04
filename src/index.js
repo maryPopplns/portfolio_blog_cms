f1b70317f5f19e0d15f0c62761c7b30bd3d02990
Spencer Knight <spencer.knightusn@gmail.com>
2022-04-03 19:26:54 -0700

    moved app to components folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from './components/app/App';
 import './reset.css';
 
 ReactDOM.render(
