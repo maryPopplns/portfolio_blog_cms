c7f4b4af94240cc9625946dac2fb11e03173ea4a
Spencer Knight <spencer.knightusn@gmail.com>
2022-04-04 11:57:29 -0700

    created test file for Navbar

---
+++ src/components/navbar/Navbar.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Navbar from './Navbar';
+
+describe('Navbar', () => {
+  // test('header includes first name', () => {
+  //   render(<Navbar />);
+  //   const firstName = screen.getByRole('heading', { name: /spencer/i });
+  //   expect(firstName).toBeInTheDocument();
+  // });
+});
