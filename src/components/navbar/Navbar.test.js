bf79603788d86cbafca09e51660fe185f09d4cec
Spencer Knight <spencer.knightusn@gmail.com>
2022-04-04 13:08:26 -0700

    navbar testing complete

--- src/components/navbar/Navbar.test.js
+++ src/components/navbar/Navbar.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Navbar from './Navbar';
 
@@ -20,5 +20,21 @@ describe('Navbar', () => {
     );
     const newPost = screen.getByRole('link', { name: /new\spost\s\+/i });
     expect(newPost).toBeInTheDocument();
+  });
+  test('home button routes to homepage', () => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
+    fireEvent(
+      screen.getByRole('link', { name: /home/i }),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    // TODO finish once the homepage is complete
+    // expect(homeLink).toBeInTheDocument();
   });
 });
