41a941eaa06d5dac5d6a85c7d62498b10d07204b
Spencer Knight <spencer.knightusn@gmail.com>
2022-04-04 18:37:56 -0700

    made inputs stateful

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,19 +1,40 @@
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { login } from '../../store/slices/loggedIn';
 import './login.css';
 
 function Login() {
-  const dispatch = useDispatch();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    console.log(username);
+    console.log(password);
+  }, [username, password]);
+
+  // const dispatch = useDispatch();
   // dispatch(login())
 
+  function loginHandler() {}
+
   return (
-    <form className='login_form'>
+    <form className='login_form' onSubmit={loginHandler}>
       <ul>
         <li>
-          <input id='username' type='text'></input>
+          <input
+            id='username'
+            type='text'
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          ></input>
         </li>
         <li>
-          <input id='password' type='password'></input>
+          <input
+            id='password'
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          ></input>
         </li>
         <div>
           <button className='login_form_submit_button'>enter</button>
