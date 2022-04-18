4681e635fd8d4aa5e4a3b1cacaec43e7308853d0
Spencer Knight <spencer.knightusn@gmail.com>
2022-04-18 09:04:40 -0700

    analysis tests complete

--- src/components/analysis/Analysis.test.js
+++ src/components/analysis/Analysis.test.js
@@ -7,8 +7,7 @@ import Analysis from './Analysis';
 import data from '../testData.json';
 
 describe('Analysis', () => {
-  const errors = data.errors;
-  const body = data.body;
+  const { errors1, errors2, body1, body2 } = data;
   const setBody = jest.fn();
   const setAnalysis = jest.fn();
 
@@ -17,9 +16,44 @@ describe('Analysis', () => {
   });
 
   test('renders save/cancel buttons', () => {
-    render(<Analysis data={{ errors, body, setBody, setAnalysis }} />);
+    render(
+      <Analysis data={{ errors: errors1, body: body1, setBody, setAnalysis }} />
+    );
 
     const save = screen.getByRole('button', { name: 'save' });
+    const cancel = screen.getByRole('button', { name: 'cancel' });
     expect(save).toBeInTheDocument();
+    expect(cancel).toBeInTheDocument();
+  });
+
+  test('clicking cancel triggers "setAnalysis', () => {
+    render(
+      <Analysis data={{ errors: errors1, body: body1, setBody, setAnalysis }} />
+    );
+
+    fireEvent(
+      screen.getByRole('button', { name: 'cancel' }),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    expect(setAnalysis).toHaveBeenCalled();
+  });
+  test('clicking save triggers "setAnalysis" and "setBody"', () => {
+    render(
+      <Analysis data={{ errors: errors2, body: body2, setBody, setAnalysis }} />
+    );
+
+    fireEvent(
+      screen.getByRole('button', { name: 'save' }),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+
+    expect(setAnalysis).toHaveBeenCalled();
+    expect(setBody).toHaveBeenCalled();
   });
 });
